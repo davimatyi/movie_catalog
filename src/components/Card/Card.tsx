42e9c06bdfeae5b79441b734571e528ea91fa295
Dávid Mátyás <jxtbv0@inf.elte.hu>
2022-01-18 20:13:37 +0100

    added ratings

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -5,28 +5,42 @@ import './Card.scss';
 
 const imageBaseUrl: string = 'https://image.tmdb.org/t/p/w500/';
 
-const Poster: FC<PosterInterface> = ({ path, fallback }): JSX.Element => {
-  if(path == null) {
+const Poster: FC<PosterInterface> = ({ path, fallback }) => {
+  if (path == null) {
     return <img src={fallback} className="card-poster" alt="Poster" />
   } else {
     return <img src={imageBaseUrl + path} className="card-poster" alt="Poster"
-    onError={({ currentTarget }) => {
-      currentTarget.onerror = null;
-      currentTarget.src = fallback;
-    }}
-  />
+      onError={({ currentTarget }) => {
+        currentTarget.onerror = null;
+        currentTarget.src = fallback;
+      }}
+    />
   }
 }
 
-const MovieCard: FC<MovieInterface> = ({ info }): JSX.Element => {
+const RatingBar: FC<{ avg: number }> = ({ avg }) => {
+  return (
+    <div className="ratings">
+      <div className="rating-box">
+        <div className="rating-bar" style={{width: avg * 10 + "px"}}></div>
+      </div>
+      <div className="rating-text">
+        {avg} / 10
+      </div>
+    </div>
+  );
+}
+
+const MovieCard: FC<MovieInterface> = ({ info }) => {
   return (
     <div className="card">
       <div className="card-left">
         <Poster path={info.poster_path} fallback={poster_fallback} />
       </div>
       <div className="card-right">
-        <div className="card-title">{ info.title }</div>
-        <div className="card-date">{ info.release_date }</div>
+        <div className="card-title">{info.title}</div>
+        <div className="card-date">{info.release_date}</div>
+        <RatingBar avg={info.vote_average} />
       </div>
       <div className="card-footer">
 
